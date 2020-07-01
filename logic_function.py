19e4a7c043859db3fd9eb67061b40574c6aac486
Joao Victor <jv.duarte.viana@gmail.com>
2020-07-01 09:59:02 -0300

    Função logical_consequence adicionado

--- logic_function.py
+++ logic_function.py
@@ -9,6 +9,55 @@ from logic_utils import *
 from logic import *
 import collections
 
+
+def logical_consequence(formula, premises):
+    Utils.verified_propositions(formula)
+    truth_table = collections.OrderedDict()
+    atoms = get_atoms(formula)
+    premises_values = []
+
+    for premise in premises:
+        if (Utils.verified_propositions(premise)
+                and not isinstance(premise, Atom)):
+            atoms = set(atoms)
+            atoms.update(get_atoms(premise))
+            atoms = sorted(atoms, key=repr)
+            truth_table.update(truth_table_generator(premise, atoms))
+            premises_values.append(truth_table[repr(premise)])
+        else:
+            raise FormulaArgumentError(premise)
+
+    if premises_values:
+        num_rows = len(premises_values[0])
+
+    for sub_formula in get_formulas(formula, atoms):
+        formula_values = (
+            _generate_values_of_formula_(
+                sub_formula,
+                num_rows,
+                atoms,
+                truth_table
+            )
+        )
+        truth_table[repr(sub_formula)] = formula_values
+
+    indexes = []
+    for row in range(num_rows):
+        premises_values_trues = (
+            [premise_value[row] for premise_value in premises_values]
+        )
+        if premises_values_trues == [True] * len(premises):
+             indexes.append(row)
+
+    is_logical_consequence = False
+    
+    if indexes:
+        is_logical_consequence = (
+            list(map(formula_values.__getitem__, indexes)) == [True] * len(indexes)
+        )
+    
+    return is_logical_consequence, truth_table
+
 def get_atoms(formula):
     Utils.verified_propositions(formula)
     return list(filter(lambda f: isinstance(f, Atom), formula.sub_formula))
@@ -17,9 +66,9 @@ def get_formulas(formula, atoms):
     Utils.verified_propositions(formula)
     return list(filter(lambda f: f not in atoms, formula.sub_formula))
 
-def truth_table_generator(formula):
+def truth_table_generator(formula, atoms=None):
     Utils.verified_propositions(formula)
-    atoms = get_atoms(formula)
+    atoms = get_atoms(formula) if atoms == None else atoms
     truth_table = collections.OrderedDict()
 
     num_rows = 2 ** len(atoms)
--- main.py
+++ main.py
@@ -8,17 +8,16 @@ from logic import *
 from logic_function import *
 
 def main():
-    p = Atom("Está chovendo", True)
-    q = Atom("A rua está molhada", True)
+    p = Atom("Está chovendo")
+    q = Atom("A rua está molhada")
     r = Atom("Aqui é um teste")
-    s = Atom("Aqui é outro teste")
-    p1 = Atom("Aqui é mais um teste")
+    
+    premise_1 = THEN(p, THEN(r, q))
+    premise_2 = THEN(p, r)
+    formula = THEN(p, q) 
 
-    p_q = AND(p, q)
-    not_p_q = NOT(p_q)
-
-    truth_table = truth_table_generator(not_p_q)
-
+    is_consequence, truth_table = logical_consequence(formula, [premise_1, premise_2])
+    print(is_consequence)
     print_truth_table(truth_table)
 
 if __name__ == "__main__":
